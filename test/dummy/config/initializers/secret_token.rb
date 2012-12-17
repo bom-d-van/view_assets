ec32a1c17350c5abe4d3a76589c8778a7270031c
bom.d.van <bom.d.van@gmail.com>
2012-12-17 10:16:54 +0800

    rails plugin test folder(unused)

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '11d5696f1871be79ae28e4f8a16693a123775b02454c6c44445239c8c8d1aede2732ea95d04b0f39c9d4857d20f3d0af0b542a10d2c20edc5f07dd9038fa61cf'
---
+++ test/view_assets_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ViewAssetsTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, ViewAssets
+  end
+end
