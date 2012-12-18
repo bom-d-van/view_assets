27bb02ad6f39477160347a6755c68748ec3ab6a9
bom.d.van <bom.d.van@gmail.com>
2012-12-18 16:02:03 +0800

    use FIXTURE_ROOT constant to store the path to fixtures

--- rspec/rspec_helper.rb
+++ rspec/rspec_helper.rb
@@ -1,3 +1,5 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib/view_assets')
+RSPEC_ROOT = File.dirname(__FILE__)
+$LOAD_PATH.unshift(RSPEC_ROOT + '/../lib/view_assets')
 require 'view_assets'
 include ViewAssets
+FIXTURE_ROOT = RSPEC_ROOT + '/fixtures'
