2c428b4efd19e9e9a18d5a8d38e494b0c7c2eb82
bom.d.van <bom.d.van@gmail.com>
2012-12-19 15:57:58 +0800

    new test fixtures

---
+++ rspec/fixtures/infused.js
@@ -0,0 +1,6 @@
+//= require_vendor vendor1, vendor2
+//= require_lib lib1, lib2, lib4
+//= require /controller1/action1, /controller/action2
+// noisy for requiring
+//= end_of_requiring
+// noisy for requiring
