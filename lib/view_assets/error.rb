861f04a8825ea271c9eac8ce5393c7038a208c24
bom.d.van <bom.d.van@gmail.com>
2012-12-26 09:17:28 +0800

    add FileNotFound error

--- lib/view_assets/error.rb
+++ lib/view_assets/error.rb
@@ -2,4 +2,5 @@ module ViewAssets
   class ConfigurationError < StandardError; end
   # class UnimplementedError < StandardError; end
   class UnknownDirectiveError < StandardError; end
+  class FileNotFound < StandardError; end
 end
