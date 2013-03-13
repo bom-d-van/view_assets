5fdb1b07e8e48521c5506c636ff0410f818155e1
bom_d_van <bom.d.van@gmail.com>
2013-03-13 14:37:17 +0800

    version update: 1.0.3

--- lib/view_assets/railtie.rb
+++ lib/view_assets/railtie.rb
@@ -2,8 +2,6 @@ module ViewAssets
   class Railtie < Rails::Railtie
     initializer "view_assets' initialzer" do
       require 'view_assets/action_view'
-      # ViewAssets::ENV = Rails.env
-      # ViewAssets::APP_ROOT = Rails.public_path
     end
 
     rake_tasks do
