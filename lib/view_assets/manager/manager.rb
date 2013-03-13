9e70cfae3b7f6b56efcebcfe5b3312553bd86be0
bom_d_van <bom.d.van@gmail.com>
2013-03-13 14:37:22 +0800

    mgr module init

---
+++ lib/view_assets/manager/manager.rb
@@ -0,0 +1,24 @@
+module ViewAssets
+  module Manager
+    require 'view_assets'
+    require 'view_assets/finder/core'
+
+    class Manager
+      def create(instruction)
+
+      end
+
+      def read(asset)
+
+      end
+
+      def update(old_asset, new_asset)
+
+      end
+
+      def remove(asset)
+
+      end
+    end
+  end
+end
